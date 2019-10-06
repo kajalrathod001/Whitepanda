8b3cf4a0a6a9f7ee1777151109ab0d0f013f6b67
kajalrathod001 <56197367+kajalrathod001@users.noreply.github.com>
2019-10-06 15:53:59 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+	body{
+		background: white;
+	}
+.fa {
+  font-size: 37px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 50%;
+  margin: 5px 30px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+
+  color: grey;
+}
+
+.fa-twitter {
+  color: grey;
+}
+
+
+.fa-linkedin {
+  color: grey;
+}
+
+
+.fa-instagram {
+  color: grey;
+}
+.copyright
+{
+	margin-top:20px;
+	font-size:16px;
+	text-align: center;
+	color:grey;
+}
+</style>
+</head>
+<body>
+  
+<div class="text-center">  
+<li class="fa fa-twitter"></li>
+<li class="fa fa-linkedin"></li>
+<li class="fa fa-instagram"></li>
+<li class="fa fa-facebook"></li>
+</div>
+<div class="copyright grey-text text-center ">
+     Copyright  ©  2019  White  Panda  Media  Pvt.  Ltd.<br>All rights reserved<br><br>
+	 <u>Privacy  policy</u>&ensp;|&ensp;<u>Terms  of  service</u>&ensp;|&ensp;<u>Service  Agreement</u>&ensp;|&ensp;<u>Contact us</u>
+</div>
+
+
+</body>
+</html> 
